b908336e16137f9b29584f82d90541b48ddae1e5
“denisneuf” <denisneuf@hotmail.com>
2015-12-19 22:46:50 +0100

    remove modules to update

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
+#!/bin/env node
 
-  console.log("App Iniciada.");
+// Set up app and DB objects
+var express    = require('express');
+console.log("App Iniciada.");
+
+var viewfolder = process.env.OPENSHIFT_REPO_DIR;
+
+if (typeof viewfolder === "undefined") {
+    
+   viewfolder = path.join(__dirname, 'views');
+   console.warn('No OPENSHIFT_REPO_DIR environment variable');
+}
+else
+{
+    
+    viewfolder = viewfolder + 'views';
+
+}
+console.warn(viewfolder);
